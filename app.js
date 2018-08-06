0bf7102fa75d36b5ac040bc11076e0a6a2c818d8
Alejandro Rios <Alx.Rios91@gmail.com>
2018-08-05 19:23:16 -0500

    added google oauth

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+//bring in auth router
+const auth = require('./routes/auth');
+
+//passport config
+require('./config/passport')(passport);
 
 app.get('/', (req, res) =>{
     res.send('It works');
 });
 
+//use auth routes
+app.use('/auth', auth);
 
 app.listen(port, () =>{
     console.log(`server started on port ${port}`);
